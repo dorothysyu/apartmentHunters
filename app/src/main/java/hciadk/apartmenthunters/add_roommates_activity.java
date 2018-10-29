4e9b048b2ccabb8e0d5cf1477e76014995358c24
dorothysyu <yu.dor@husky.neu.edu>
2018-10-28 23:37:17 -0400

    up to features checklists

---
+++ app/src/main/java/hciadk/apartmenthunters/add_roommates_activity.java
@@ -0,0 +1,13 @@
+package hciadk.apartmenthunters;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class add_roommates_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_roommates_activity);
+    }
+}
