6938b65d8a480ca073eabc2bad506ebf3876a7cf
dorothysyu <yu.dor@husky.neu.edu>
2018-10-29 13:42:57 -0400

    basic basic basic basic skeleton

---
+++ app/src/main/java/hciadk/apartmenthunters/ExtraNotesActivity.java
@@ -0,0 +1,13 @@
+package hciadk.apartmenthunters;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ExtraNotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_extra_notes);
+    }
+}
---
+++ app/src/main/java/hciadk/apartmenthunters/IndividualChecklistActivity.java
@@ -0,0 +1,13 @@
+package hciadk.apartmenthunters;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IndividualChecklistActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_individual_checklist);
+    }
+}
