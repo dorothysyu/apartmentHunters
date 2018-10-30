f671c4b929c0fdeffb285fc6b1b6ebf7570c5348
Kaylien Phan <kaylienphan@gmail.com>
2018-10-30 17:19:03 -0400

    this is the backend branch

--- app/src/main/java/hciadk/apartmenthunters/MainActivity.java
+++ app/src/main/java/hciadk/apartmenthunters/MainActivity.java
@@ -19,5 +19,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //for creating new branch on github
     }
 }
