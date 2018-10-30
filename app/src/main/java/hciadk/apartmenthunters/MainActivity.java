44957f3368e03cf7c44964db9f6571e454c243e2
Kaylien Phan <kaylienphan@gmail.com>
2018-10-30 17:40:17 -0400

    added IDs to activity_main

--- app/src/main/java/hciadk/apartmenthunters/MainActivity.java
+++ app/src/main/java/hciadk/apartmenthunters/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TableRow;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +20,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //for creating new branch on github
+        TableRow aptRow1 = findViewById(R.id.apt1);
+        TableRow aptRow2 = findViewById(R.id.apt2);
+        TableRow aptRow3 = findViewById(R.id.apt3);
+
+        aptRow1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,
+                        ApartmentEditActivity.class));
+            }
+        });
+
+        aptRow2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,
+                        ApartmentEditActivity.class));
+            }
+        });
+
+        aptRow3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,
+                        ApartmentEditActivity.class));
+            }
+        });
     }
 }
