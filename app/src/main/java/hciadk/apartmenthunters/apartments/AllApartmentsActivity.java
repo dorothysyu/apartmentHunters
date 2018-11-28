0dc9ce38e2215838e5440358af3a1d0a8119a17d
Kaylien Phan <kaylienphan@gmail.com>
2018-11-28 04:27:21 -0500

    need to checkout

--- app/src/main/java/hciadk/apartmenthunters/apartments/AllApartmentsActivity.java
+++ app/src/main/java/hciadk/apartmenthunters/apartments/AllApartmentsActivity.java
@@ -24,9 +24,9 @@ public class AllApartmentsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_apts);
 
-        TableRow aptRow1 = findViewById(R.id.apt1);
-        TableRow aptRow2 = findViewById(R.id.apt2);
-        TableRow aptRow3 = findViewById(R.id.apt3);
+        final TableRow aptRow1 = findViewById(R.id.apt1);
+        final TableRow aptRow2 = findViewById(R.id.apt2);
+        final TableRow aptRow3 = findViewById(R.id.apt3);
         ImageButton add = findViewById(R.id.add_new_apt);
 
         aptRow1.setOnClickListener(new View.OnClickListener() {
@@ -60,6 +60,35 @@ public class AllApartmentsActivity extends AppCompatActivity {
                         AddApartmentActivity.class));
             }
         });
+
+        aptRow1.setVisibility(View.INVISIBLE);
+        aptRow2.setVisibility(View.INVISIBLE);
+        aptRow3.setVisibility(View.INVISIBLE);
+
+
+        add.setOnClickListener(new View.OnClickListener() {
+            boolean isApt1Vis = false;
+            boolean isApt2Vis = false;
+            boolean isApt3Vis = false;
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AllApartmentsActivity.this, AddApartmentActivity.class);
+                if(isApt1Vis) {
+                    aptRow1.setVisibility(View.VISIBLE);
+                    isApt1Vis = true;
+                }
+                else if (isApt2Vis) {
+                    aptRow2.setVisibility(View.VISIBLE);
+                    isApt2Vis = true;
+                }
+                else if (isApt3Vis) {
+                    aptRow3.setVisibility(View.VISIBLE);
+                    isApt3Vis = true;
+                }
+
+            }
+        });
+
 
 /*        back.setOnClickListener(new View.OnClickListener() {
             @Override
