45d44e9a2ed292909267624083e7e9f4b8a1cdd7
Kaylien Phan <kaylienphan@gmail.com>
2018-11-28 04:16:27 -0500

    able to add new address for apartment

--- app/src/main/java/hciadk/apartmenthunters/apartments/AllApartmentsActivity.java
+++ app/src/main/java/hciadk/apartmenthunters/apartments/AllApartmentsActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TableRow;
 
 import hciadk.apartmenthunters.R;
@@ -26,7 +27,7 @@ public class AllApartmentsActivity extends AppCompatActivity {
         TableRow aptRow1 = findViewById(R.id.apt1);
         TableRow aptRow2 = findViewById(R.id.apt2);
         TableRow aptRow3 = findViewById(R.id.apt3);
-        Button back = findViewById(R.id.back_to_ind_apt);
+        ImageButton add = findViewById(R.id.add_new_apt);
 
         aptRow1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,6 +50,14 @@ public class AllApartmentsActivity extends AppCompatActivity {
             public void onClick(View v) {
                 startActivity(new Intent(AllApartmentsActivity.this,
                         ApartmentEditActivity.class));
+            }
+        });
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(AllApartmentsActivity.this,
+                        AddApartmentActivity.class));
             }
         });
 
